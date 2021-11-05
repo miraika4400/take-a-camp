c70c49eb8f675c6cf5014f6f34cd63ce1db935f8
yosidayuto <yuu-poi-921@i.softbank.jp>
2021-11-05 10:31:42 +0900

    リザルトの塗りスコア表示処理生成

--- 01_project/take a camp/take a camp/score_paint.cpp
+++ 01_project/take a camp/take a camp/score_paint.cpp
@@ -1,6 +1,6 @@
 //===================================================
 //
-//    キルスコアクラスの処理[score_kill.cpp]
+// 塗りスコア表示処理 [score_paint.cpp]
 //    Author : 吉田 悠人
 //
 //====================================================
@@ -8,26 +8,27 @@
 //**********************************
 //インクルード
 //**********************************
-#include "score_kill.h"
 #include "number.h"
 #include "chara_select.h"
-#include "kill_count.h"
+#include "score_paint.h"
+#include "color_tile.h"
 
 //**********************************
 //インクルード
 //**********************************
 #define SCORE_SIZE		(D3DXVECTOR3(40.0f, 30.0f, 0.0f))
-#define CREATE_POS_Y	(500.0f)
+#define CREATE_POS_Y	(400.0f)
 #define SCORE_SPACE		(SCREEN_WIDTH/4)
+
 //==================================
 // コンストラクタ
 //==================================
-CScoreKill::CScoreKill()
+CScorePaint::CScorePaint()
 {
 	//初期化
 	memset(m_apNumber, 0, sizeof(m_apNumber));		// ナンバーのクリア
 	memset(m_pos, 0, sizeof(m_pos));// 位置
-	for (int nPlayer=0; nPlayer<MAX_PLAYER; nPlayer++)
+	for (int nPlayer = 0; nPlayer<MAX_PLAYER; nPlayer++)
 	{
 		m_col[nPlayer] = D3DXCOLOR(0.8f, 0.3f, 0.8f, 1.0f);	// カラー
 	}
@@ -37,40 +38,40 @@ CScoreKill::CScoreKill()
 //==================================
 // デストラクタ
 //==================================
-CScoreKill::~CScoreKill()
+CScorePaint::~CScorePaint()
 {
 }
 
 //==================================
 // クリエイト
 //==================================
-CScoreKill * CScoreKill::Create(void)
+CScorePaint * CScorePaint::Create(void)
 {
 	// メモリの確保
-	CScoreKill *pScoreKill = NULL;
-	pScoreKill = new CScoreKill;
+	CScorePaint *pScorePaint = NULL;
+	pScorePaint = new CScorePaint;
 
 	//NULLチェック
-	if (pScoreKill != NULL)
+	if (pScorePaint != NULL)
 	{
 		// 初期化処理
-		pScoreKill->Init();
+		pScorePaint->Init();
 		// オブジェクトタイプの設定
-		pScoreKill->SetPriority(OBJTYPE_UI);
+		pScorePaint->SetPriority(OBJTYPE_UI);
 	}
-	return pScoreKill;
+	return pScorePaint;
 }
 
 //==================================
 // 初期化処理
 //==================================
-HRESULT CScoreKill::Init(void)
-{	
+HRESULT CScorePaint::Init(void)
+{
 	// プレイヤー人数取得
 	m_nPlayerNum = CCharaSelect::GetEntryPlayerNum();
 
 	// X軸生成用変数
-	float fPosX = (SCREEN_WIDTH / 2) - (((float)(m_nPlayerNum - 1) * SCORE_SPACE) / 2) + ((SCORE_SIZE.x/2)*(MAX_KILL_SCORE_DIGIT - 1));
+	float fPosX = (SCREEN_WIDTH / 2) - (((float)(m_nPlayerNum - 1) * SCORE_SPACE) / 2) + ((SCORE_SIZE.x / 2)*(MAX_PAINT_SCORE_DIGIT - 1));
 
 	// 最大桁数分ループ
 	for (int nPlayer = 0; nPlayer<MAX_PLAYER; nPlayer++)
@@ -78,7 +79,7 @@ HRESULT CScoreKill::Init(void)
 		//プレイ人数分生成
 		if (CCharaSelect::GetEntryData(nPlayer).bEntry)
 		{
-			for (int nCntDigit = 0; nCntDigit < MAX_KILL_SCORE_DIGIT; nCntDigit++)
+			for (int nCntDigit = 0; nCntDigit < MAX_PAINT_SCORE_DIGIT; nCntDigit++)
 			{
 				m_apNumber[nPlayer][nCntDigit] = CNumber::Create(0,
 					D3DXVECTOR3((float)(fPosX - nCntDigit * (SCORE_SIZE.x)), CREATE_POS_Y, 0.0f),
@@ -94,7 +95,7 @@ HRESULT CScoreKill::Init(void)
 	{
 		if (CCharaSelect::GetEntryData(nPlayer).bEntry)
 		{
-			SetKillScore(nPlayer, CKillCount::GetTotalKill(nPlayer));
+			SetPaintScore(nPlayer, CColorTile::GetTileNum(nPlayer));
 		}
 	}
 
@@ -104,11 +105,11 @@ HRESULT CScoreKill::Init(void)
 //==================================
 // 終了処理
 //==================================
-void CScoreKill::Uninit(void)
+void CScorePaint::Uninit(void)
 {
 	for (int nPlayer = 0; nPlayer < MAX_PLAYER; nPlayer++)
 	{
-		for (int nCntDigit = 0; nCntDigit < MAX_KILL_SCORE_DIGIT; nCntDigit++)
+		for (int nCntDigit = 0; nCntDigit < MAX_PAINT_SCORE_DIGIT; nCntDigit++)
 		{
 			if (m_apNumber[nPlayer][nCntDigit] != NULL)
 			{
@@ -126,11 +127,11 @@ void CScoreKill::Uninit(void)
 //==================================
 // 更新処理
 //==================================
-void CScoreKill::Update(void)
+void CScorePaint::Update(void)
 {
 	for (int nPlayer = 0; nPlayer < MAX_PLAYER; nPlayer++)
 	{
-		for (int nCntDigit = 0; nCntDigit < MAX_KILL_SCORE_DIGIT; nCntDigit++)
+		for (int nCntDigit = 0; nCntDigit < MAX_PAINT_SCORE_DIGIT; nCntDigit++)
 		{
 			//NULLチェック
 			if (m_apNumber[nPlayer][nCntDigit] != NULL)
@@ -145,11 +146,11 @@ void CScoreKill::Update(void)
 //==================================
 // 描画処理
 //==================================
-void CScoreKill::Draw(void)
+void CScorePaint::Draw(void)
 {
 	for (int nPlayer = 0; nPlayer < MAX_PLAYER; nPlayer++)
 	{
-		for (int nCntDigit = 0; nCntDigit < MAX_KILL_SCORE_DIGIT; nCntDigit++)
+		for (int nCntDigit = 0; nCntDigit < MAX_PAINT_SCORE_DIGIT; nCntDigit++)
 		{
 			//NULLチェック
 			if (m_apNumber[nPlayer][nCntDigit] != NULL)
@@ -163,11 +164,11 @@ void CScoreKill::Draw(void)
 //=============================
 // スコア計算処理
 //=============================
-void CScoreKill::SetKillScore(int nPlayer, int nScore)
+void CScorePaint::SetPaintScore(int nPlayer, int nScore)
 {
 	int nInteger = 0;//計算用変数
 
-	for (int nCntDigit = 0; nCntDigit < MAX_KILL_SCORE_DIGIT; nCntDigit++)
+	for (int nCntDigit = 0; nCntDigit < MAX_PAINT_SCORE_DIGIT; nCntDigit++)
 	{
 		nInteger = int(nScore / pow(10, nCntDigit + 1)); //1の位以下を切り捨てる
 		nInteger = int(nInteger * pow(10, nCntDigit + 1));
--- 01_project/take a camp/take a camp/score_paint.h
+++ 01_project/take a camp/take a camp/score_paint.h
@@ -1,13 +1,13 @@
 //=============================================================================
 //
-// キルスコア表示処理 [score_kill.h]
+// 塗りスコア表示処理 [score_paint.h]
 // Author : 吉田 悠人
 //
 //=============================================================================
 
 //二重インクルード防止
-#ifndef _SCORE_KILL_H_
-#define _SCORE_KILL_H_
+#ifndef _SCORE_PAINT_H_
+#define _SCORE_PAINT_H_
 
 //*****************************
 //インクルード
@@ -24,35 +24,36 @@ class CNumber;
 //*****************************
 //マクロ定義
 //*****************************
-#define MAX_KILL_SCORE_DIGIT (3)  // スコアの最大桁数
+#define MAX_PAINT_SCORE_DIGIT (3)  // スコアの最大桁数
 
 //*****************************
 //クラス定義
 //*****************************
 
 //スコアクラス
-class CScoreKill : public CScene
+class CScorePaint : public CScene
 {
 public:
 	//============
 	// メンバ関数
 	//============
-	CScoreKill();
-	~CScoreKill();
+	CScorePaint();
+	~CScorePaint();
 
-	static CScoreKill *Create(void); // クラス生成
+	static CScorePaint *Create(void); // クラス生成
 
 	HRESULT Init(void);		// 初期化
 	void	Uninit(void);	// 終了
 	void	Update(void);	// 更新
 	void	Draw(void);		// 描画
-	void	SetKillScore(int nPlayer, int nScore);	// スコア
+	void	SetPaintScore(int nPlayer, int nScore);	// スコア
 
 private:
 	//============
 	// メンバ変数
 	//============
-	CNumber* m_apNumber[MAX_PLAYER][MAX_KILL_SCORE_DIGIT];	// ナンバーポリゴン
+	CNumber* m_apNumber[MAX_PLAYER][MAX_PAINT_SCORE_DIGIT];	// ナンバーポリゴン
+	int			m_nPaintScore[MAX_PLAYER];					// スコア
 	D3DXVECTOR3 m_pos[MAX_PLAYER];							// 位置
 	D3DXCOLOR	m_col[MAX_PLAYER];							// カラー
 	int			m_nPlayerNum;								// プレイヤー人数取得用変数
